5cc1f5adfeb42bf09e8d397ccd7ec3572e248571
sjudass <i.a.pichuev@gmail.com>
2018-11-08 10:31:22 +0300

    Creating page layout
    Создание верстки страницы

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="ru-RU"
+<head>
+    <meta charset="UTF-8"
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Агентство РАРИО</title>
+    <link href="css/site.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body >
+<div id="fullpage">
+    <div class="site-wrapper section" data-anchor="block1" style="background-image: url(images/cover.jpg); background-size: cover;">
+        <div class="site-wrapper-inner">
+            <div class="cover-container">
+                <div class="masthead clearfix">
+                    <div class="inner">
+                        <h3 class="masthead-brand">РАРИО</h3>
+                        <ul id="menu">
+                            <li data-menuanchor="block1"><a href="#block1">Главная</a></li>
+                            <li data-menuanchor="block2"><a href="#block2">Деятельность</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="inner cover">
+                    <h1 class="cover-heading">Агентство развития информационного общества РАРИО</h1>
+                    <p class="lead">
+                        <br><a href="#" class="btn btn-lg btn-default">Узнать больше</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="advantages section" data-anchor="block2">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+                <li data-target="#myCarousel" data-slide-to="4"></li>
+                <li data-target="#myCarousel" data-slide-to="5"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="first-slide" src="images/strategy.jpg" alt="First slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1 class="activity">СФЕРЫ ДЕЯТЕЛЬНОСТИ</h1>
+                            <h1>Разработка и экспертиза</h1>
+                            <p>Государственных целевых программ, стратегий, концепций</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="second-slide" src="images/organization.jpg" alt="Second slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1 class="activity">СФЕРЫ ДЕЯТЕЛЬНОСТИ</h1>
+                            <h1>Мероприятия</h1>
+                            <p>Организация форумов, конгрессов, саммитов, конференций и семинаров</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="third-slide" src="images/analyze.jpg" alt="Third slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1 class="activity">СФЕРЫ ДЕЯТЕЛЬНОСТИ</h1>
+                            <h1>Исследования</h1>
+                            <p>Международные, федеральные и региональные исследования, анализ рынков, маркетинговые исследования</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="third-slide" src="images/GR.jpg" alt="Third slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1 class="activity">СФЕРЫ ДЕЯТЕЛЬНОСТИ</h1>
+                            <h1>GR-проекты</h1>
+                            <p>Реализация комплексных совместных проектов с органами власти, государственными компаниями</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="third-slide" src="images/NIR.jpg" alt="Third slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1 class="activity">СФЕРЫ ДЕЯТЕЛЬНОСТИ</h1>
+                            <h1>НИР и НИОКР</h1>
+                            <p>Инновационные разработки и исследования любой сложности, получение грантов, поиск соинвестора</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="third-slide" src="images/business.jpg" alt="Third slide">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1 class="activity">СФЕРЫ ДЕЯТЕЛЬНОСТИ</h1>
+                            <h1>Разработки</h1>
+                            <p>Инновационных систем и портальных решений нового поколения, внедрение сервисов электронной демократии</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; Агентство развития информационного общества РАРИО <?= date('Y') ?></p>
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/jquery.fullPage.min.js"></script>
+<script src="js/script.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            anchors: ['block1', 'block2', 'block3', 'block4'],
+            menu: '#menu',
+            css3: true,
+            scrollingSpeed: 1000
+        });
+    });
+</script>
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,6 @@
+/**
+ * Created by antoh on 28.10.2018.
+ */
+$(.'carousel').carousel({
+    interval: 300;
+})
